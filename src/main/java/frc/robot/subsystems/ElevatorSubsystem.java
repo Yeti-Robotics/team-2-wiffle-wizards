a59c08e66c1da91031b5625c3ac92559cf070983
DL <thedanyaliu@gmail.com>
2024-11-12 20:35:45 -0500

    Added lower lift trigger and fixed commands for door and elevator.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -81,13 +81,9 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     // Method to move the elevator down
     public void moveDown(double speed) {
-        if (!bottomLimitSwitch.get()) {
-            elevatorMotor1.set(-Math.abs(speed));
-            elevatorMotor2.set(-Math.abs(speed));
+        elevatorMotor1.set(-Math.abs(speed));
+        elevatorMotor2.set(-Math.abs(speed));
 
-        } else {
-            stop();
-        }
     }
 
     // Stop the elevator motor
@@ -116,21 +112,14 @@ public class ElevatorSubsystem extends SubsystemBase {
         return topLimitSwitch.get();
     }
 
-    private Command moveDownAndStop(double speed){
-        return startEnd(() -> moveDown(speed), this::stop);
+    public Command moveDownAndStop(double speed){
+        return startEnd(() -> moveDown(speed), this::stop).until(this::isAtBottom);
     }
 
-    private Command moveUpAndStop(double speed){
-        return startEnd(() -> moveUp(speed), this::stop);
+    public Command moveUpAndStop(double speed){
+        return startEnd(() -> moveUp(speed), this::stop).until(this::isAtTop);
     }
 
-    public Command goDown(double speed) {
-        return runOnce(()-> moveDownAndStop(speed).until(this::isAtBottom));
-    }
-
-    public Command goUp(double speed) {
-        return runOnce(()-> moveUpAndStop(speed).until(this::isAtTop));
-    }
 
     @Override
     public void periodic() {
